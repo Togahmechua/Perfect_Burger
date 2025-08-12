c82f2a2b6e4a1b7d62b2ff4a0b2dc8c2a890025f
Togahmechua <136262133+Togahmechua@users.noreply.github.com>
2025-08-12 12:02:52 +0700

    UI Update

---
+++ Assets/_Game/Script/Optimize/CacheString.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CacheString : MonoBehaviour
+{
+    public static string TAG_LVBTN = "IdleLevelBtn";
+    public static string TAG_Clicked_StartCanvas = "Clicked_StartCanvas";
+}
