ab1535e9b3385a0f04654dd71690bbb2d03bf853
Togahmechua <136262133+Togahmechua@users.noreply.github.com>
2025-08-14 15:10:25 +0700

    Reapply "Full Game Update"
    This reverts commit 24ab2d3bd64fc25d512001590912629962b2a689.

--- Assets/_Game/Script/Manager/AudioManager.cs
+++ Assets/_Game/Script/Manager/AudioManager.cs
@@ -14,11 +14,9 @@ public class AudioManager : Singleton<AudioManager>
     public AudioClip backgroundClip;
     public AudioClip click;
     public AudioClip loose;
-    public AudioClip coin;
+    public AudioClip correct;
     public AudioClip hurt;
     public AudioClip dead;
-    public AudioClip horn;
-    public AudioClip drift;
 
     private bool isMuted;
 
@@ -26,9 +24,9 @@ public class AudioManager : Singleton<AudioManager>
 
     private void Start()
     {
-        /*musicSource.clip = backgroundClip;
+        musicSource.clip = backgroundClip;
         musicSource.Play();
-        TurnOn();*/
+        TurnOn();
     }
 
     public void PlaySFX(AudioClip clip)
